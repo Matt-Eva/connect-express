cad74378fcb694b218bdadc5af8dba7a82c946d6
Matt Eva <matteweva@gmail.com>
2023-11-14 12:50:35 -0800

    set up dummy auth to login specific user

--- src/index.js
+++ src/index.js
@@ -9,12 +9,23 @@ io.on("connection", async (socket) =>{
     console.log(socket.request.session.user)
 })
 
-app.post("/login", (req, res) =>{
-    req.session.user = true
-    console.log(req.session.id)
-    res.status(200).send({user: req.session.user})
-})
+app.post("/login", async (req, res) =>{
+    const body = req.body
+    const session = driver.session()
+    try {
+        const query = 'MATCH (user:User {name: $name}) RETURN user'
 
-app.get("/me", (req, res) =>{
-    console.log(req.session.id)
+        const result = await session.executeRead( async tx => tx.run(query, {name: body.name}))
+
+        const user = result.records[0].get("user").properties
+
+        res.status(200).send(user)
+    } catch (e){
+        console.error(e)
+
+        res.status(500).send({error: true})
+    } finally {
+        await session.close()
+    }
+
 })
