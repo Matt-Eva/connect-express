5f356a7866b1869cf4560b76b9875675c6bf9af3
Matt Eva <matteweva@gmail.com>
2023-11-27 13:59:10 -0800

    set up create account route and completed account creation

--- src/index.js
+++ src/index.js
@@ -98,8 +98,6 @@ app.post("/login", async (req, res) =>{
         } else {
             res.status(401).send({error: "unauthorized"})
         }
-
-        
     } catch (e){
         console.error(e)
 
@@ -126,6 +124,55 @@ app.get("/me", (req, res) =>{
         res.status(200).send(req.session.user)
     } else {
         res.status(401).send({error: "unauthorized"})
+    }
+})
+
+app.post("/new-account", async (req, res) => {
+    console.log(req.body)
+    const session = driver.session()
+    try {
+        const password = await argon2.hash(req.body.password)
+        const user = await session.executeWrite(async tx =>{
+            const existingUser = await tx.run(`
+                MATCH (u:User {email: $email}) RETURN u
+            `, {email: req.body.email})
+            if (existingUser.records.length !== 0){
+                return "already exists"
+            }
+
+            const newUser = await tx.run(`
+            CREATE (u:User {email: $email, password: $password, name: $name, firstName: $firstName, lastName: $lastName, profileImg: $profileImg, uId: $uId}) RETURN u.email AS email, u.name AS name, u.firstName AS firstName, u.lastName AS lastName, u.profileImg AS profileImg, u.uId AS uId
+            `, {
+                email: req.body.email,
+                password: password,
+                name: req.body.name,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                profileImg: req.body.profileImg,
+                uId: uuid()
+            })
+            const newUserRecord = newUser.records[0]
+            return {
+                email: newUserRecord.get("email"),
+                name: newUserRecord.get("name"),
+                firstName: newUserRecord.get("firstName"),
+                lastName: newUserRecord.get("lastName"),
+                profileImg: newUserRecord.get("profileImg"),
+                uId: newUserRecord.get("uId")
+            }
+        })
+        if (user === "already exists"){
+            res.status(422).send({error: "email already in use"})
+        } else{
+            req.session.user = user
+            console.log(req.session.user)
+            res.status(201).send(user)
+        }
+    } catch(e){
+        console.error(e)
+        res.status(500).send({error: "internal server error"})
+    }finally {
+       await session.close()
     }
 })
 
